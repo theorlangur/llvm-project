1723607a1183a10766bfa0f3862fcb7f870dec5e
Dmytro Dukov <dmitry.dukov@malighting.de>
2021-06-12 15:59:36 +0200

    basic changes to add support for compilation database dependencies
    changed patch for JSON CDB:
      dependencies are not included into the common index (and not returned
      with getAllFiles call), they have own match and index member variables
      and those are checked in getCompileCommands
    
    now it should not return -nan score and break vscode side

--- clang/lib/Tooling/ExpandResponseFilesCompilationDatabase.cpp
+++ clang/lib/Tooling/ExpandResponseFilesCompilationDatabase.cpp
@@ -34,6 +34,10 @@ public:
     return Base->getAllFiles();
   }
 
+  std::vector<std::string> getAllFilesWithDeps() const override {
+    return Base->getAllFilesWithDeps();
+  }
+
   std::vector<CompileCommand>
   getCompileCommands(StringRef FilePath) const override {
     return expand(Base->getCompileCommands(FilePath));
