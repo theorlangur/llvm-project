eff39a391fa9dfb3c79dbf0c87086b7668849245
Dmytro Dukov <dmitry.dukov@malighting.de>
2021-06-13 09:36:24 +0200

    working on preamble alternative for header files
    introduced dynamic pch: is created and managed by PCH manager
    only for opened documents
    idea is to increase speed of code completions in headers
    __CLANGD_DYNAMIC_PCH__ should be defined before
    --include in command as an indicator of target

--- clang-tools-extra/clangd/ClangdServer.cpp
+++ clang-tools-extra/clangd/ClangdServer.cpp
@@ -338,6 +338,12 @@ void ClangdServer::addDocument(PathRef File, llvm::StringRef Contents,
   Opts.PreambleParseForwardingFunctions = PreambleParseForwardingFunctions;
   Opts.ImportInsertions = ImportInsertions;
 
+  {
+    auto CC = CDB.getCompileCommand(File);
+    if (CC)
+		PrecompiledHeaderMgr->tryAddDynamicPCH(*CC, draftVFS);
+  }
+
   // Compile command is set asynchronously during update, as it can be slow.
   ParseInputs Inputs;
   Inputs.TFS = &getHeaderFS();
@@ -452,6 +458,11 @@ ClangdServer::createConfiguredContextProvider(const config::Provider *Provider,
 void ClangdServer::removeDocument(PathRef File) {
   DraftMgr.removeDraft(File);
   WorkScheduler->remove(File);
+  {
+    auto CC = CDB.getCompileCommand(File);
+    if (CC)
+		PrecompiledHeaderMgr->tryRemoveDynamicPCH(*CC);
+  }
 }
 
 void ClangdServer::codeComplete(PathRef File, Position Pos,
