b98c7f13e088f1ad4f46e4b029e83f6eb9666b24
Dmytro Dukov <dmitry.dukov@malighting.de>
2021-06-13 09:33:52 +0200

    PCHManager notifies now via event when a PCH was built
    so opened documents are updated automatically

--- clang-tools-extra/clangd/support/Function.h
+++ clang-tools-extra/clangd/support/Function.h
@@ -92,6 +92,8 @@ public:
     assert(Listeners.empty());
   }
 
+  unsigned subscribed() const { return ListenerCount; }
+
 private:
   static_assert(std::is_same<std::decay_t<T>, T>::value,
                 "use a plain type: event values are always passed by const&");
