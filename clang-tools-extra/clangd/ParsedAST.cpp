50936c42ccf98b9457a478b215cb3c07b693fddc
Dmytro Dukov <dmitry.dukov@malighting.de>
2021-06-13 09:27:25 +0200

    passing correctly draftfs to PCHManager while building
    passing draftfs also when calling ParsedAST::build
    this allows to be able to code-complete in cpp without saving changes in
    header and it also produces no errors regarding wrong size of pch

--- clang-tools-extra/clangd/ParsedAST.cpp
+++ clang-tools-extra/clangd/ParsedAST.cpp
@@ -392,6 +392,12 @@ ParsedAST::build(llvm::StringRef Filename, const ParseInputs &Inputs,
   SPAN_ATTACH(Tracer, "File", Filename);
 
   auto VFS = Inputs.TFS->view(Inputs.CompileCommand.Directory);
+  if (Inputs.DraftFS) {
+    IntrusiveRefCntPtr<llvm::vfs::OverlayFileSystem> Overlay(
+        new llvm::vfs::OverlayFileSystem(VFS)); 
+    Overlay->pushOverlay(Inputs.DraftFS);      
+    VFS = Overlay;
+  }
   if (PCHAccess && *PCHAccess) 
   {
     Preamble = nullptr;
