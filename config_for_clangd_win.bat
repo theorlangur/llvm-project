28a0e260bbf31909f7444a69824891c4091a3ec6
Dmytro Dukov <dmitry.dukov@malighting.de>
2023-03-19 12:31:21 +0100

    added lto options, but frankly these don't really show a runtime improvement

--- config_for_clangd_win.bat
+++ config_for_clangd_win.bat
@@ -1,1 +1,1 @@
-cmake -Hllvm -Bbuilddbg -A x64 -T ClangCL -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86"
+cmake -Hllvm -Bbuildwin -A x64 -T ClangCL -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_USE_LINKER=lld -DLLVM_ENABLE_LTO=Full
