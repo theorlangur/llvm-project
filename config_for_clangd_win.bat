cb2cab34c993aad9ec37d0dbe0f2e17d0c7a00bd
Dmytro Dukov <dmitry.dukov@malighting.de>
2023-09-03 05:36:16 +0200

    weirdly enough linking with LTO and lld causes crashes...

--- config_for_clangd_win.bat
+++ config_for_clangd_win.bat
@@ -1,1 +1,2 @@
-cmake -Hllvm -Bbuildwin -A x64 -T ClangCL -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_USE_LINKER=lld -DLLVM_ENABLE_LTO=Full
+rem cmake -Hllvm -Bbuildwin -A x64 -T ClangCL -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_USE_LINKER=lld -DLLVM_ENABLE_LTO=Full
+cmake -Hllvm -Bbuildwin -A x64 -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86"
