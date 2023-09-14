3d10c91d4b35ced93d1691a0dbf63f4004fcad09
Dmytro Dukov <dmitry.dukov@malighting.de>
2023-09-14 20:11:36 +0200

    making sure related PCH and related other data live long enough while AST built with it is alive

--- clang-tools-extra/clangd/ParsedAST.cpp
+++ clang-tools-extra/clangd/ParsedAST.cpp
@@ -755,6 +755,8 @@ ParsedAST::build(llvm::StringRef Filename, const ParseInputs &Inputs,
                    std::move(Diags), std::move(Includes));
   llvm::move(getIncludeCleanerDiags(Result, Inputs.Contents),
              std::back_inserter(Result.Diags));
+  if (PCHAccess)//make sure VFS lives long enough
+    Result.PCHAccessLifeTime = std::move(*PCHAccess);
   return std::move(Result);
 }
 
