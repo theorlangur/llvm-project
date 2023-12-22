3e4b6dde8ae9c66e9b82fb94a23eeed9150697b8
Dmytro Dukov <dmitry.dukov@malighting.de>
2023-12-22 15:24:07 +0100

    added another line for configuring, for dbg

--- config_for_clangd_win.bat
+++ config_for_clangd_win.bat
@@ -1,2 +1,3 @@
 rem cmake -Hllvm -Bbuildwin -A x64 -T ClangCL -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_USE_LINKER=lld -DLLVM_ENABLE_LTO=Full
-cmake -Hllvm -Bbuildwin -A x64 -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86"
+rem cmake -Hllvm -Bbuildwin_msvc2 -A x64 -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86"
+cmake -Hllvm -Bbuildwin_msvc_dbg -A x64 -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86"
