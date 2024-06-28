041a7f598fccff7c429cc04a02ddb11ae2dbe7ca
Dmytro Dukov <dmitry.dukov@malighting.de>
2024-06-28 21:46:04 +0200

    added a command line to run config for mid2024

--- config_for_clangd_win.bat
+++ config_for_clangd_win.bat
@@ -1,3 +1,4 @@
 rem cmake -Hllvm -Bbuildwin -A x64 -T ClangCL -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_USE_LINKER=lld -DLLVM_ENABLE_LTO=Full
 rem cmake -Hllvm -Bbuildwin_msvc2 -A x64 -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86"
-cmake -Hllvm -Bbuildwin_msvc_dbg -A x64 -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86"
+rem cmake -Hllvm -Bbuildwin_msvc_dbg -A x64 -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86"
+cmake -Hllvm -Bbuildwin_mid2024 -A x64 -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86"
