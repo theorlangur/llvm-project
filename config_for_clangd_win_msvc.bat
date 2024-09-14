2e12e3da3c7c1b768290d2fc07f8c84b16d8d5d1
Dmytro Dukov <dmytro.dukov@malighting.de>
2024-09-14 15:39:47 +0200

    added another win config batch file

--- config_for_clangd_win_msvc.bat
+++ config_for_clangd_win_msvc.bat
@@ -4,4 +4,4 @@ rem cmake -Hllvm -Bbuildwin_msvc_dbg -A x64 -DLLVM_ENABLE_PROJECTS="clang;clang-
 rem cmake -Hllvm -Bbuildwin_mid2024 -A x64 -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86"
 rem cmake -Hllvm -A x64 -TClangCL -Bbuildwin_clang_mid2024 -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_ENABLE_LTO=Full
 rem cmake -Hllvm -Bbuildwin_clang_mid2024 -A x64 -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_USE_SANITIZER="Address"
-cmake -Hllvm -A x64 -TClangCL -Bbuildwin_rel -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_ENABLE_LTO=Full
+cmake -Hllvm -A x64 -Bbuildwin_msvc -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86"
