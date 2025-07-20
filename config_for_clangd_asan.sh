2efa5767bec3bd97681f9a3e981462c33f52a31f
TheOrlangur <theorlangur@gmail.com>
2025-07-20 21:09:00 +0200

    added a couple of scripts for configuration

---
+++ config_for_clangd_asan.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cmake -Hllvm -Bbuild_asan \
+    -DCMAKE_CXX_FLAGS="-fsanitize=address" \
+    -DCMAKE_C_FLAGS="-fsanitize=address" \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" \
+    -DLLVM_TARGETS_TO_BUILD="X86" \
+    -DCMAKE_C_COMPILER=clang \
+    -DCMAKE_CXX_COMPILER=clang++ \
+    -DLLVM_USE_LINKER=lld
+
