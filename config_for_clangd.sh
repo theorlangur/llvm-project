e91e8451d5eb9b4c105592db39ecb5d45e8c3363
TheOrlangur <theorlangur@gmail.com>
2025-08-17 11:22:07 +0200

    more config adjustments (AVX2)

--- config_for_clangd.sh
+++ config_for_clangd.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-cmake -Hllvm -Bbuild -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_USE_LINKER=lld -DLLVM_ENABLE_LTO=Full
+cmake -Hllvm -Bbuild \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_FLAGS="-march=x86-64-v3" \
+    -DCMAKE_C_FLAGS="-march=x86-64-v3" \
+    -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" \
+    -DLLVM_TARGETS_TO_BUILD="X86" \
+    -DCMAKE_C_COMPILER=clang \
+    -DCMAKE_CXX_COMPILER=clang++ \
+    -DLLVM_USE_LINKER=lld \
+    -DLLVM_ENABLE_LTO=Full
--- config_for_clangd_win.bat
+++ config_for_clangd_win.bat
@@ -4,4 +4,4 @@ rem cmake -Hllvm -Bbuildwin_msvc_dbg -A x64 -DLLVM_ENABLE_PROJECTS="clang;clang-
 rem cmake -Hllvm -Bbuildwin_mid2024 -A x64 -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86"
 rem cmake -Hllvm -A x64 -TClangCL -Bbuildwin_clang_mid2024 -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_ENABLE_LTO=Full
 rem cmake -Hllvm -Bbuildwin_clang_mid2024 -A x64 -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_USE_SANITIZER="Address"
-cmake -Hllvm -A x64 -TClangCL -Bbuildwin_clang_lto -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_ENABLE_LTO=Full
+cmake -Hllvm -A x64 -TClangCL -Bbuildwin_clang_lto -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_ENABLE_LTO=Full -DCMAKE_CXX_FLAGS="-march=x86-64-v3" -DCMAKE_C_FLAGS="-march=x86-64-v3"
