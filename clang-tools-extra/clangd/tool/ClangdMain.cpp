a7ad5c8f91e6ec4a217a52770e5a63f26e5dbedf
Dmytro Dukov <dmytro.dukov@malighting.de>
2025-08-24 21:59:28 +0200

    bumped PCH ver to 1.06 (tweaks seems to work as expected)

--- clang-tools-extra/clangd/tool/ClangdMain.cpp
+++ clang-tools-extra/clangd/tool/ClangdMain.cpp
@@ -807,7 +807,7 @@ enum class ErrorResultCode : int {
   CheckFailed = 3
 };
 
-static const char *PCHVers = "PCH ver. 1.05";
+static const char *PCHVers = "PCH ver. 1.06";
 int clangdMain(int argc, char *argv[]) {
   // Clang could run on the main thread. e.g., when the flag '-check' or '-sync'
   // is enabled.
